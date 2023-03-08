6e8404d8219d12a8032999bbdb28bfcb85766499
MichaelLoka <62102221+MichaelLoka@users.noreply.github.com>
2023-03-08 16:49:56 +0200

    Add files via upload

--- contracts/System.sol
+++ contracts/System.sol
@@ -1,18 +1,11 @@
 pragma solidity >=0.4.17<0.9.0;
 
-contract System {
-
+contract Company_Con{
     struct Company {
         string name;
         string email;
         string password;
         uint256 certifcate_num;
-    }
-
-    struct User {
-        string name;
-        string email;
-        string password;
     }
 
     struct Product {
@@ -22,16 +15,8 @@ contract System {
         string companyName;
     }
 
-    Product public prod;
-
-    mapping(address => User) users;
     mapping(address => Company) companies;
     mapping(bytes32 => Product) public products;
-
-
-    function register_User(address _useraddress,string memory _name, string memory _email, string memory _password) public {
-        users[_useraddress] = User(_name,_email,_password);
-    }
 
     function register_Company(address _compaddress,string memory _name, string memory _email, string memory _password,uint256 _cert_num) public {
         companies[_compaddress] = Company(_name,_email,_password,_cert_num);
@@ -44,21 +29,9 @@ contract System {
         products[p_hashed] = p;
     }
 
-    function login_user (address user_add , string memory _name , string memory _password) public view returns (bool found)
-    {
-            User memory user = users[user_add];
-            
-            // str compare keccak256(bytes(a)) == keccak256(bytes(b));
-            if(keccak256(bytes(user.name)) == keccak256(bytes(_name)) && keccak256(bytes(user.password)) == keccak256(bytes(_password)))
-                return true;
-            else
-                return false;
-    }
-
     function login_company (address company_add , string memory _name , string memory _password) public view returns (bool found)
     {
             Company memory company = companies[company_add];
-            
             // str compare keccak256(bytes(a)) == keccak256(bytes(b));
             if(keccak256(bytes(company.name)) == keccak256(bytes(_name)) && keccak256(bytes(company.password)) == keccak256(bytes(_password)))
                 return true;
@@ -66,14 +39,45 @@ contract System {
                 return false;
     }
 
-    function verify_product (bytes32 product_add) public returns (Product memory prodd)
-    {
-        prod = products[product_add];
-        return prod;
+    function get_product (bytes32 product_add) external view returns (Product memory Prod){
+        return products[product_add];
     }
 
     function delete_product (bytes32 product_add) public {
         delete products[product_add];
     }
-    
+}
+
+contract User_Con {
+    struct User {
+        string name;
+        string email;
+        string password;
+    }
+
+    Company_Con public _company;
+    mapping(address => User) users;
+
+    function register_User(address _useraddress,string memory _name, string memory _email, string memory _password) public {
+        users[_useraddress] = User(_name,_email,_password);
+    }
+
+    function login_user (address user_add , string memory _name , string memory _password) public view returns (bool found)
+    {
+            User memory user = users[user_add];
+            if(keccak256(bytes(user.name)) == keccak256(bytes(_name)) && keccak256(bytes(user.password)) == keccak256(bytes(_password)))
+                return true;
+            else
+                return false;
+    }
+
+    function verify_product (bytes32 product_add) external view returns (bool legit)
+    {
+        if(_company.get_product(product_add).id == 0){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
 }
